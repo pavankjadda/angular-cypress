6a563d8fdadaf7bf851db19d1570a4f5e2af0aed
Pavan K Jadda <17564080+pavankjadda@users.noreply.github.com>
2021-02-22 23:36:06 -0500

    Update and rename spec.ts to home.e2e.spec.ts

---
+++ cypress/integration/home.e2e.spec.ts
@@ -0,0 +1,18 @@
+describe('Home Page Test', () =>
+{
+  beforeEach(function () {
+    cy.visit('/');
+  });
+
+  it('Show Error If Failed to show message', () => {
+
+    // Check if the word Welcome exists
+    cy.contains('Welcome');
+    
+    cy.find('tutorial').click();
+
+
+  });
+
+});
+
